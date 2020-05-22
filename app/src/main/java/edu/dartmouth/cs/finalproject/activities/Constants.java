e9aca110530a99e456ffa611d9d75e57752e2ffa
Sylvester Elorm Coch <sylvester.e.coch.22@dartmouth.edu>
2020-05-21 21:11:53 -0500

    UI with horizontal scrollview and navigation drawer

---
+++ app/src/main/java/edu/dartmouth/cs/finalproject/activities/Constants.java
@@ -0,0 +1,6 @@
+package edu.dartmouth.cs.finalproject.activities;
+
+public class Constants {
+    /* Device permissions REQUEST CODE */
+    public static final int REQUEST_CODE_CAMERA_PERMISSION = 1;
+}
