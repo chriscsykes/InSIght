faab2603c574bd7655135b152482ac70bfeca71d
Chris Sykes <chrissykes@Chriss-MacBook-Pro-2.local>
2020-05-21 22:26:01 -1000

    update

--- app/src/main/java/edu/dartmouth/cs/finalproject/activities/IntroActivity.java
+++ app/src/main/java/edu/dartmouth/cs/finalproject/activities/IntroActivity.java
@@ -20,33 +20,37 @@ public class IntroActivity extends AppIntro {
         addSlide(AppIntroFragment.newInstance(
                 getString(R.string.welcome_slide),
                 getString(R.string.welcome_string),
-                R.mipmap.ic_launcher,
-                ContextCompat.getColor(getApplicationContext(),
-                        R.color.slide)));
+                R.drawable.app_logo_2,
+                ContextCompat.getColor(getApplicationContext(), R.color.slide),
+                Color.BLACK,
+                Color.BLACK));
 
         // slide 2: text-to-speech
         addSlide(AppIntroFragment.newInstance(
                 getString(R.string.text_to_speech_slide),
                 getString(R.string.text_to_speech_string),
-                R.mipmap.ic_launcher,
-                ContextCompat.getColor(getApplicationContext(),
-                        R.color.slide)));
+                R.drawable.text_to_speech,
+                ContextCompat.getColor(getApplicationContext(), R.color.slide),
+                Color.BLACK,
+                Color.BLACK));
 
         // slide 3: barcode scanner
         addSlide(AppIntroFragment.newInstance(
                 getString(R.string.barcode_scanner_slide),
                 getString(R.string.barcode_scanner_string),
-                R.mipmap.ic_launcher,
-                ContextCompat.getColor(getApplicationContext(),
-                        R.color.slide)));
+                R.drawable.barcode_scanner_logo,
+                ContextCompat.getColor(getApplicationContext(), R.color.slide),
+                Color.BLACK,
+                Color.BLACK));
 
         // slide 4: image recognition
         addSlide(AppIntroFragment.newInstance(
                 getString(R.string.image_recognition_slide),
                 getString(R.string.image_recognition_string),
-                R.mipmap.ic_launcher,
-                ContextCompat.getColor(getApplicationContext(),
-                        R.color.slide)));
+                R.drawable.image_recognition_logo,
+                ContextCompat.getColor(getApplicationContext(), R.color.slide),
+                Color.BLACK,
+                Color.BLACK));
     }
 
     @Override
